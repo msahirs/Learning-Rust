2d9cfdbcf1d358f1395dc91cf13df25b34fea673
ssujahudeen <sujahudeen.sahir@gmail.com>
2022-11-12 13:07:09 +0100

    bob

---
+++ WebsiteTutorials/GettingStarted/Hello_World/main.rs
@@ -0,0 +1,26 @@
+/*
+To Execute, do the following in powershell or terminal (Windows)
+
+rustc main.rs
+.\main.exe
+*/
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+/*
+The main function is special: it is always the first code that runs in every executable
+Rust program. Here, the first line declares a function named main that has no parameters 
+and returns nothing. If there were parameters, they would go inside the parentheses ().
+
+The function body is wrapped in {}.
+Rust requires curly brackets around all function bodies.
+It’s good style to place the opening curly bracket on the same
+line as the function declaration, adding one space in between.
+*/
