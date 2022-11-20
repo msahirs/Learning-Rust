9aaab3f613ef1070ecf235fd429429691eeb00a7
ssujahudeen <sujahudeen.sahir@gmail.com>
2022-11-20 19:58:35 +0100

    updated variables

--- WebsiteTutorials/variables/src/main.rs
+++ WebsiteTutorials/variables/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
-    let mut x = 5;
+    let x = 5;
 
     let x = x + 2;
 
     println!("The value of x is: {x}");
 
-    let x = 6;
+    let mut x = 6;
 
     {
         let x = x * 2;
